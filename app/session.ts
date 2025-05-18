4772f9c8800b7467dc9cd0ab7c1f96101780e73f
Patrick <67339475+P-K-D@users.noreply.github.com>
2025-05-18 10:21:27 -0500

    feat: externalize session secret

--- app/session.ts
+++ app/session.ts
@@ -1,11 +1,15 @@
 import { createCookieSessionStorage } from '@remix-run/cloudflare'
+import invariant from 'tiny-invariant'
+
+const SESSION_SECRET = process.env.SESSION_SECRET
+invariant(SESSION_SECRET, 'SESSION_SECRET must be set')
 
 export const { getSession, commitSession, destroySession } =
 	createCookieSessionStorage({
 		// a Cookie from `createCookie` or the same CookieOptions to create one
 		cookie: {
 			name: '__session',
-			secrets: ['oooOOooOOoOOoOOOOoo'],
+                       secrets: [SESSION_SECRET],
 			sameSite: true,
 			httpOnly: true,
 		},
